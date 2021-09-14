b19233f3455ebb83703210865ecc53a0c3235e90
Sanjay-2001 <90711694+Sanjay-2001@users.noreply.github.com>
2021-09-14 21:26:27 +0530

    Day 1 assignment submitted

---
+++ Day1_assignment.py
@@ -0,0 +1,18 @@
+#Question no. 1
+a="hello"
+b="hello"
+print(a is b)
+c=89.3
+d=78.4
+print(c is d)
+
+#Question no. 2
+print(1>3>4)
+
+#Question no. 3
+str='100'
+print("int('100') with base 2 = ", int(str,2))
+s=str
+print("value of str function = ",s)
+float_string=float("-12")
+print("value of float function = ",float_string)
